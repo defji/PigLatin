d894f631cafa75bcf190b4a654a4b2887c7fb98e
Györkös Balázs <balazs.gyorkos@gmail.com>
2017-04-05 15:43:08 +0200

    init

--- src/Translator.php
+++ src/Translator.php
@@ -4,10 +4,67 @@ namespace PigLatin;
 
 class Translator
 {
-    public static function translate($word)
+
+    public function __construct($inputText,$useDash=FALSE)
     {
+        $this->dash = $useDash ? '-':'';
+        $this->input = $inputText;
+    }
 
-        return substr($word,1).'-'.substr($word,0,1).'ay';
+    protected $consonants = [
+        'b','c','d','f','g','h','j','k','l','m','n','p','q','r','s','t','v','w','x','z',
+        'B','C','D','F','G','H','J','K','L','M','N','P','Q','R','S','T','V','W','X','Z'
+    ];
+    protected $vowels = [
+        'a','e','i','o','u','y',
+        'A','E','I','O','U','Y',
+    ];
 
+    protected $marks = [
+        '.', ',', '?', '!', ';', ':', '-'
+    ];
+
+    public function translate()
+    {
+        $translated = "";
+        $words = explode(" ",$this->input);
+        foreach($words as $word) {
+            $translated.= $this->translateWord($word)." ";
+        }
+        return $translated;
+    }
+
+
+    private function translateWord($word)
+    {
+        $arr = str_split($word);
+        list($first) = $arr;
+        list($last) = array_reverse($arr);
+
+        $mark="";
+        if(in_array($last,$this->marks)) {
+            $mark=$last;
+            $word = substr($word,-1);
+        }
+
+        $counter = 0;
+        $translation = "";
+        $suffix = "";
+
+
+
+        if(in_array($first,$this->consonants)) {
+            while (in_array($arr[$counter], $this->consonants)) {
+                $suffix .= $arr[$counter];
+                $counter++;
+            }
+            $translation .= substr($word, $counter);
+            $translation .= $this->dash.$suffix.'ay';
+        } else {
+            $suffix .= $this->dash.'way';
+            $translation .= $word . $suffix;
+        }
+
+        return $translation.$mark;
     }
 }
--- tests/test.php
+++ tests/test.php
@@ -4,6 +4,20 @@ require_once __DIR__ . '/../vendor/autoload.php';
 
 
 use PigLatin\Translator;
+use Bmitch\PiglatinTranslator;
 
-echo Translator::translate('duck');
+
+$randomtext = "Eat imagine you chiefly few end ferrars compass. Be visitor females am ferrars inquiry. Latter law remark two lively thrown. Spot set they know rest its. Raptures law diverted believed jennings consider children the see. Had invited beloved carried the colonel. Occasional principles discretion it as he unpleasing boisterous. She bed sing dear now son half.";
+//$randomtext = "eagle question star happy three beast";
+
+
+$t = new Translator($randomtext,false);
+echo $t->translate();
+print "\n------\n";
+
+
+$t = new PiglatinTranslator();
+print $t->translate($randomtext);
+//
+
 
