26088939869718781a606f29b08711d3b46d8c15
defji <balazs.gyorkos@gmail.com>
2017-11-22 11:13:21 +0000

    Apply fixes from StyleCI

--- src/Translator.php
+++ src/Translator.php
@@ -1,53 +1,52 @@
-<?php 
+<?php
 
 namespace defji\PigLatin;
 
 class Translator
 {
-
-    public function __construct($useDash=FALSE)
+    public function __construct($useDash = false)
     {
-        $this->dash = $useDash ? '-':'';
+        $this->dash = $useDash ? '-' : '';
     }
 
     protected $consonants = [
-        'b','c','d','f','g','h','j','k','l','m','n','p','q','r','s','t','v','w','x','z',
-        'B','C','D','F','G','H','J','K','L','M','N','P','Q','R','S','T','V','W','X','Z'
+        'b', 'c', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'm', 'n', 'p', 'q', 'r', 's', 't', 'v', 'w', 'x', 'z',
+        'B', 'C', 'D', 'F', 'G', 'H', 'J', 'K', 'L', 'M', 'N', 'P', 'Q', 'R', 'S', 'T', 'V', 'W', 'X', 'Z',
     ];
     protected $vowels = [
-        'a','e','i','o','u','y',
-        'A','E','I','O','U','Y',
+        'a', 'e', 'i', 'o', 'u', 'y',
+        'A', 'E', 'I', 'O', 'U', 'Y',
     ];
 
     protected $marks = [
-        '.', ',', '?', '!', ';', ':', '-'
+        '.', ',', '?', '!', ';', ':', '-',
     ];
 
     public function translate($input)
     {
-        $translated = "";
-        $words = explode(" ",$input);
-        foreach($words as $word) {
-            $translated.= $this->translateWord($word)." ";
+        $translated = '';
+        $words = explode(' ', $input);
+        foreach ($words as $word) {
+            $translated .= $this->translateWord($word).' ';
         }
-        return substr($translated,0,-1);
-    }
 
+        return substr($translated, 0, -1);
+    }
 
     public function translateWord($word)
     {
         $arr = str_split($word);
         list($first) = $arr;
         list($last) = array_reverse($arr);
-        $mark="";
-        if(in_array($last,$this->marks)) {
-            $mark=$last;
-            $word = substr($word,0,-1);
+        $mark = '';
+        if (in_array($last, $this->marks)) {
+            $mark = $last;
+            $word = substr($word, 0, -1);
         }
         $counter = 0;
-        $translation = "";
-        $suffix = "";
-        if(in_array($first,$this->consonants)) {
+        $translation = '';
+        $suffix = '';
+        if (in_array($first, $this->consonants)) {
             while (in_array($arr[$counter], $this->consonants)) {
                 $suffix .= $arr[$counter];
                 $counter++;
@@ -56,8 +55,9 @@ class Translator
             $translation .= $this->dash.$suffix.'ay';
         } else {
             $suffix .= $this->dash.'way';
-            $translation .= $word . $suffix;
+            $translation .= $word.$suffix;
         }
+
         return $translation.$mark;
     }
 }
--- tests/test.php
+++ tests/test.php
@@ -2,20 +2,20 @@
 
 use defji\PigLatin\Translator;
 
-class PigTest extends PHPUnit_Framework_TestCase {
-
+class PigTest extends PHPUnit_Framework_TestCase
+{
     public function test_sentence_with_dash()
     {
-        $inputText = "All their equipment and instruments are alive.";
-        $referenceText = "All-way eir-thay equipment-way and-way instruments-way are-way alive-way.";
+        $inputText = 'All their equipment and instruments are alive.';
+        $referenceText = 'All-way eir-thay equipment-way and-way instruments-way are-way alive-way.';
         $pig = new Translator(true);
         $this->assertEquals($referenceText, $pig->translate($inputText));
     }
 
     public function test_sentence_without_dash()
     {
-        $inputText = "All their equipment and instruments are alive.";
-        $referenceText = "Allway eirthay equipmentway andway instrumentsway areway aliveway.";
+        $inputText = 'All their equipment and instruments are alive.';
+        $referenceText = 'Allway eirthay equipmentway andway instrumentsway areway aliveway.';
         $pig = new Translator(false);
         $this->assertEquals($referenceText, $pig->translate($inputText));
     }
@@ -23,18 +23,16 @@ class PigTest extends PHPUnit_Framework_TestCase {
     public function test_words()
     {
         $words = [
-            "beast"=>"east-bay",
-            "dough"=>"ough-day",
-            "happy"=>"appy-hay",
-            "question"=>"uestion-qay",
-            "star"=>"ar-stay",
-            "three"=>"ee-thray"
+            'beast'   => 'east-bay',
+            'dough'   => 'ough-day',
+            'happy'   => 'appy-hay',
+            'question'=> 'uestion-qay',
+            'star'    => 'ar-stay',
+            'three'   => 'ee-thray',
         ];
         $pig = new Translator(true);
-        foreach($words as $input => $reference) {
+        foreach ($words as $input => $reference) {
             $this->assertEquals($reference, $pig->translateWord($input));
         }
     }
 }
-
-
